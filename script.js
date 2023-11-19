eded8a5200aa1bcb39eb0cdb133cdff257e12492
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-19 14:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(let i=0;i<s1.length;i++){
+		if(s1.slice(i,i+s2.length+1).toUpperCase()==s2.toUpperCase()){
+			return i;
+		}
+	}
+	return -1;
 }
 
-// Please do not change the code below
+// Please do not change the code belo
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
